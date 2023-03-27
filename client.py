b15801717277e88a72b3520b941e5fbbd7dfbd5d
iamrebecca <93889338+iamrebecca@users.noreply.github.com>
2023-03-27 17:39:35 -0400

    Create client.py

---
+++ client.py
@@ -0,0 +1,48 @@
+import socket
+import threading
+import time
+
+
+HOST = '35.3.207.175'  # The server's hostname or IP address
+PORT = 65432        # The port used by the server
+
+
+def process_data_from_server(x):
+    x1, y1 = x.split(",")
+    return x1,y1
+
+
+def my_client():
+    threading.Timer(11, my_client).start()
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        while 1:
+
+            """
+            my = input("Enter command ")
+
+            my_inp = my.encode('utf-8')
+
+            s.sendall(my_inp)
+            """
+
+            my = "Data"
+
+            my_inp = my.encode('utf-8')
+
+            s.sendall(my_inp)
+            data = s.recv(1024).decode('utf-8')
+
+            x_temperature,y_humidity = process_data_from_server(data)
+
+            print("Temperature {}".format(x_temperature))
+            print("Humidity {}".format(y_humidity))
+
+        #s.close()
+            #time.sleep(5)
+
+
+if __name__ == "__main__":
+    
+    my_client()
